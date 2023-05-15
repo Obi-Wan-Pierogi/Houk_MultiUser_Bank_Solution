7f0ca983ba5696ddd4b23f757957aa2ee1d13dd9
leeho <leeho@DESKTOP-L00PKD4>
2023-05-15 09:29:04 -0700

    Add project files.

---
+++ Houk_MultiUser_Bank_Project/Bank.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Houk_MultiUser_Bank
+{
+    internal class Bank
+    {
+        private const decimal _startBalance = 10000;
+        private decimal _balance;
+        private decimal _limit = 500M;
+
+        public string[] user = { "jlennon", "pmccartney", "gharrison", "rstarr" };
+        public string[] password = { "johnny", "pauly", "georgy", "ringoy" };
+        public decimal[] balance = { 1250M, 2500M, 3000M, 1000M };
+
+        public Bank()
+        {
+            _balance = _startBalance;
+        }
+
+        public string GetBalance()
+        {
+            return "The banks overall cash on hand is " + BankBalance.ToString("C");
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                amount = 0;
+            }
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > _limit && _balance >= _limit)
+            {
+                amount = _limit;
+            }
+            if (amount < 0)
+            {
+                amount = 0;
+            }
+            if (amount > _balance)
+            {
+                amount = _balance;
+            }
+            _balance -= amount;
+        }
+        public decimal CustomerWithdraw(int currentUser, decimal amount)
+        {
+            int i = currentUser;
+            if (amount > _limit && balance[i] >= _limit)
+            {
+                amount = _limit;
+            }
+            if (amount < 0)
+            {
+                amount = 0;
+            }
+            if (amount > balance[i])
+            {
+                amount = balance[i];
+            }
+            balance[i] -= amount;
+            return amount;
+        }
+
+        public decimal CustomerDeposit(int currentUser, decimal amount)
+        {
+            int i = currentUser;
+            if (amount < 0)
+            {
+                amount = 0;
+            }
+            balance[i] += amount;
+            return amount;
+        }
+
+        public string CustomerBalance(int currentUser)
+        {
+            int i = currentUser;
+            return "Your current balance is " + balance[i].ToString("C");
+        }
+        public string Error()
+        {
+            return "Withdrawals are limited to " + _limit.ToString("C");
+        }
+
+        public decimal BankBalance
+        {
+            get { return _balance; }
+        }
+
+        public decimal Limit
+        {
+            get { return _limit; }
+        }
+    }
+}
---
+++ Houk_MultiUser_Bank_Project/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+// Lee Houk
+// IT112
+// NOTES: 
+// BEHAVIORS NOT IMPLEMENTED AND WHY: None
+
+namespace Houk_MultiUser_Bank
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank Bank = new Bank();
+            Menu(Bank);
+        }
+        
+        static void Menu(Bank Bank)
+        {
+            bool loggedin = true;
+            string username;
+            string password;
+            int currentUser = -1;
+                        
+            do
+            {
+                Console.WriteLine("Welcome to the bank, please choose from the following options.");
+                Console.WriteLine("1) Log in");
+                Console.WriteLine("2) Exit the app");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Please log in to continue.");
+                        Console.WriteLine("Enter your username: ");
+                        username = Console.ReadLine();
+                        Console.WriteLine("Enter your password: ");
+                        password = Console.ReadLine();
+                        
+                        for(int i = 0; i < Bank.user.Length; i++)
+                        {
+                            if (username == Bank.user[i])
+                            {
+                                if (password == Bank.password[i])
+                                {
+                                    loggedin = true;
+                                    currentUser = i;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                loggedin= false;
+                            }
+                        }
+                        if (!loggedin)
+                        {
+                            Console.WriteLine("Your username or password is incorrect.");                                                        
+                        }
+                        while (loggedin)
+                        {                            
+                            decimal amount = 0;
+                            if (Bank.balance[currentUser] > 0)
+                            {
+                                Console.WriteLine("Please choose from the listed options:");
+                                Console.WriteLine("1) Make a deposit");
+                                Console.WriteLine("2) Make a withdrawal");
+                                Console.WriteLine("3) Check balance");
+                                Console.WriteLine("4) Log out");
+                                Console.WriteLine("\r\nSelect an option:");
+
+                                switch (Console.ReadLine())
+                                {
+                                    case "1":
+                                        Console.WriteLine("How much would you like to deposit?");
+                                        amount = decimal.Parse(Console.ReadLine());
+                                        Bank.Deposit(Bank.CustomerDeposit(currentUser, amount));
+                                        Console.WriteLine(Bank.CustomerBalance(currentUser) + "\n" + Bank.GetBalance() + "\n");
+                                        break;
+                                    case "2":
+                                        Console.WriteLine("How much would you like to withdraw?");
+                                        amount = decimal.Parse(Console.ReadLine());
+                                        if (amount <= Bank.Limit)
+                                        {
+                                            Bank.Withdraw(Bank.CustomerWithdraw(currentUser, amount));
+                                            Console.WriteLine(Bank.CustomerBalance(currentUser) + "\n" + Bank.GetBalance() + "\n");
+                                            loggedin = true;
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("\n" + Bank.Error());
+                                            Bank.Withdraw(Bank.CustomerWithdraw(currentUser, amount));
+                                            Console.WriteLine(Bank.CustomerBalance(currentUser) + "\n" + Bank.GetBalance() + "\n");
+                                            loggedin = true;
+                                            break;
+                                        }
+                                    case "3":
+                                        Console.WriteLine(Bank.CustomerBalance(currentUser) + "\n");
+                                        loggedin = true;
+                                        break;                                        
+                                    case "4":
+                                        loggedin= false;
+                                        currentUser = -1;
+                                        break;
+                                    default:
+                                        loggedin = true;
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please choose from the listed options:");
+                                Console.WriteLine("1) Make a deposit");
+                                Console.WriteLine("2) Check balance");
+                                Console.WriteLine("3) Log out");
+                                Console.WriteLine("\r\nSelect an option:");
+
+                                switch (Console.ReadLine())
+                                {
+                                    case "1":
+                                        Console.WriteLine("How much would you like to deposit?");
+                                        amount = decimal.Parse(Console.ReadLine());
+                                        Bank.Deposit(Bank.CustomerDeposit(currentUser, amount));
+                                        Console.WriteLine(Bank.CustomerBalance(currentUser) + "\n" + Bank.GetBalance() + "\n");
+                                        loggedin = true;
+                                        break;
+                                        //return true;
+                                    case "2":
+                                        Console.WriteLine(Bank.CustomerBalance(currentUser) + "\n");
+                                        loggedin = true;
+                                        break;
+                                        //return true;
+                                    case "3":
+                                        loggedin = false;
+                                        break;
+                                        //return true;
+                                    default:
+                                        loggedin = true;
+                                        break;
+                                        //return true;
+                                }
+                            }
+                        }
+                        break;
+                    case "2":
+                        Environment.Exit(0);
+                        break;
+                }
+            }while (!loggedin);
+        }                                                                  
+    }
+}
